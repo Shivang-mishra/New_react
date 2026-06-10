96413503e7cc69564df1e96ca1be4e202621710e
Shivang Mishra <your-email@example.com>
2026-06-10 15:58:13 +0530

    advance-routes

--- 19_routing-advance/src/components/Navbar.jsx
+++ 19_routing-advance/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex py-4 items-center px-8 bg-cyan-900 justify-between'>
-        <h2 className='text-2xl font-bold'>Sheryians</h2>
+        <h2 className='text-2xl font-bold'>SHIVANG</h2>
         <div className='flex gap-10'>
           <Link className='text-lg font-medium' to='/'>Home</Link>
           <Link className='text-lg font-medium' to='/about'>About</Link>
